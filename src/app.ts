fd885f910330039c138a88d3b0d7b3054991305e
Lelberto <jsurieux@gmail.com>
2020-11-26 10:45:34 +0100

    Database connection implementation Add app code in anonymous function because there is async functions Add logging and error catching

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,29 @@
+import database from './database';
 import expressConfig from './express-config';
 import dotenv from 'dotenv';
 
-dotenv.config();
-const app = expressConfig();
+(async function() {
+    try {
+        // .env configuration
+        dotenv.config();
 
-// TODO Register routers here with app.use()
+        // Database connection
+        await database.connect(process.env.DB_URL);
+        console.log(`Connected to database "${process.env.DB_URL}"`);
 
-// Handler used when no endpoint matches
-app.all('*', (req, res) => {
-    return res.status(404).json({ error: `Unknown endpoint ${req.method} ${req.originalUrl}` });
-});
+        // Express configuration
+        const app = expressConfig();
 
-app.listen(80);
+        // TODO Register routers here with app.use()
+
+        // Handler used when no endpoint matches
+        app.all('*', (req, res) => {
+            return res.status(404).json({ error: `Unknown endpoint ${req.method} ${req.originalUrl}` });
+        });
+
+        app.listen(process.env.API_PORT as unknown as number);
+        console.log(`Server listening on port ${process.env.API_PORT}`);
+    } catch (err) {
+        console.error(err);
+    }
+})();
