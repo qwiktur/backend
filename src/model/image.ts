fe3f0f008d8032446d7e93274e1f4020a9ab99b3
Lelberto <jsurieux@gmail.com>
2020-11-27 15:02:55 +0100

    Image validations and refactoring

--- src/model/image.ts
+++ src/model/image.ts
@@ -1,32 +1,37 @@
 import mongoose, { Document } from 'mongoose'
 import { BaseAttributes } from './model';
-import { ThemeModel } from './theme';
+import Theme, { ThemeModel } from './theme';
+
 const Schema = mongoose.Schema;
 
 export interface ImageModel extends BaseAttributes, Document{
-   src:string,
-   title:string,
-   theme:ThemeModel
+    src: string;
+    title: string;
+    theme: ThemeModel;
 }
 
 const imageSchema = new Schema({
- src: {
-  type: String,
-  required: true,
-  trim: true
- },
- title:{
-    type:String,
-    required:true,
-    trim:true
- },
- theme: {
-  type: Schema.Types.ObjectId,
-  ref: "theme",
-  required: true
- }
-}, {
-   timestamps: true
+    src: {
+        type: Schema.Types.String,
+        required: [true, 'Source is required'],
+        trim: true
+    },
+    title: {
+        type: Schema.Types.String,
+        required: [true, 'Title is required'],
+        trim: true
+    },
+    theme: {
+        type: Schema.Types.ObjectId,
+        ref: 'theme',
+        required: [true, 'Theme is required'],
+        validate: {
+            validator: (themeId: string) => Theme.exists({ _id: themeId }),
+            message: 'Invalid theme'
+        }
+    }
+},  {
+    timestamps: true
 });
 
 const Image = mongoose.model<ImageModel>('image', imageSchema);
