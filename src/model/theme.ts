b2e2253fc3265e7f04056e081c83d5ab81a28980
Lelberto <jsurieux@gmail.com>
2020-11-27 15:13:12 +0100

    Add theme validations Refactoring

--- src/model/theme.ts
+++ src/model/theme.ts
@@ -1,19 +1,21 @@
 import mongoose, {Document} from 'mongoose'
 import { BaseAttributes } from './model';
+
 const Schema = mongoose.Schema;
 
 export interface ThemeModel extends BaseAttributes, Document {
     name: string;
 }
+
 const themeSchema = new Schema({
- name: {
-  type: String,
-  required: true,
-  trim: true
- }
+    name: {
+        type: Schema.Types.String,
+        required: [true, 'Name is required'],
+        trim: true
+    }
 }, {
     timestamps: true
- });
+});
 
 const Theme = mongoose.model<ThemeModel>('theme', themeSchema);
 
