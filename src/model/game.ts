3e07a990b807e390954e6a2593723095db469e4c
Lelberto <jsurieux@gmail.com>
2020-11-27 12:19:33 +0100

    Add validations and subschemas

--- src/model/game.ts
+++ src/model/game.ts
@@ -11,42 +11,68 @@ export interface GameModel extends Document{
 }
 
 export interface GameQuestion{
-        target:QuestionModel,
-        history:[{
-            user:UserModel
-            time:Number,
-            correct:Boolean
-        }]
+    target: QuestionModel,
+    history?: [{
+        user:UserModel
+        time:number,
+        correct:boolean
+    }]
+}
+
+const historySubSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    time: {
+        type: Number
+    },
+    correct: {
+        type: Boolean
     }
+}, {
+    _id: false
+});
+
+const questionSubSchema = new Schema({
+    target: {
+        type: Schema.Types.ObjectId,
+        ref: 'question',
+        required: [true, 'Question target is required']
+    },
+    history: {
+        type: [{
+            type: historySubSchema
+        }],
+        default: []
+    }
+}, {
+    _id: false
+});
 
 const gameSchema = new Schema({
- 
- theme: {
-  type: Schema.Types.ObjectId,
-  ref: "theme"
- },
- players:[{
-     type:Schema.Types.ObjectId,
-     ref:"user"
- }],
- questions:[{
-     target:{
-        type:Schema.Types.ObjectId,
-        ref:"question"
+    theme: {
+        type: Schema.Types.ObjectId,
+        ref: 'theme',
+        required: [true, 'Theme is required']
     },
-    history:[{
-        user:{
-            type:Schema.Types.ObjectId,
-            ref:"user"
-        },
-        time:{
-            type:Number
-        },
-        correct:{
-            type:Boolean
-        }
-    }]
- }]
+    players: {
+        type: [{
+            type: Schema.Types.ObjectId,
+            ref:'user',
+        }],
+        required: [true, 'Players are required'],
+        validate: [{
+            validator: (playerIds: string) => playerIds.length >= 2,
+            message: '2 players minimum'
+        }]
+    },
+    questions: {
+        type: [{
+            type: questionSubSchema
+        }],
+        required: [true, 'Questions are required']
+    }
 });
 
 const Game = mongoose.model<GameModel>('game', gameSchema);
