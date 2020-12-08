a0357bbc3c136a14ecc8c733a7a69ca9e8a4929e
Lelberto <jsurieux@gmail.com>
2020-12-08 14:48:40 +0100

    Theme controller refactoring

--- src/controllers/themeController.ts
+++ src/controllers/themeController.ts
@@ -1,70 +1,55 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import Theme from '../model/theme';
+import { ErrorResponse } from '../util/errors';
 
-export const createTheme = async (req: Request, res:Response):Promise<Response> =>{
-    try{
+export const createTheme = async (req: Request, res: Response): Promise<Response> => {
+    try {
         const theme = await Theme.create({
-            name:req.body.name
+            name: req.body.name
         });
-        return res.status(201).send(theme)
-    }catch(err){
-        return err
+        return res.status(201).send({ id: theme.id });
+    } catch (err) {
+        return res.status(500).send({ error: 'server_error', error_description: 'Internal server error' } as ErrorResponse);
     }
 }
 
-export const getThemes = async (req:Request, res:Response):Promise<Response> => {
-    try{
-        const themes = await Theme.find({});
-            res.status(200).json({
-                data: themes
-            });
-        }catch(err){
-            return res.status(500).send(err)
-        }
-   }
-   
-   export const getOneTheme = async (req:Request, res:Response):Promise<Response> => {
-        try {
-        const themeId = req.params.themeId;
-        const theme = await Theme.findById(themeId);
-        if (!theme){
-            return res.status(404).send('Theme not found')
-        } 
-        res.status(200).json({
-        data: theme
-        });
-        } catch (err) {
-            return res.status(500).send(err)
-        }
-   }
+export const getThemes = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        return res.status(200).json(await Theme.find());
+    } catch (err) {
+        return res.status(500).send({ error: 'server_error', error_description: 'Internal server error' } as ErrorResponse);
+    }
+}
 
-   export const updateTheme = async (req:Request, res:Response):Promise<Response> => {
-        try {
-            const update = req.body
-            const themeId = req.params.themeId;
-            await Theme.findByIdAndUpdate(themeId, update);
-            const theme = await Theme.findById(themeId)
-            res.status(200).json({
-            data: theme,
-            message: 'Theme has been updated'
-            });
-        } catch (err) {
-            return res.status(500).send(err)
+export const getOneTheme = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const theme = await Theme.findById(req.params.themeId);
+        if (theme == null){
+            return res.status(404).send({ error: 'not_found', error_description: 'Theme not found' } as ErrorResponse);
         }
-   }
+        return res.status(200).json({ theme });
+    } catch (err) {
+        return res.status(500).send({ error: 'server_error', error_description: 'Internal server error' } as ErrorResponse);
+    }
+}
 
-   export const deleteTheme = async (req:Request, res: Response):Promise<Response>=>{
-       try{
-        const themeId = req.params.themeId;
-        const theme = await Theme.findByIdAndDelete(themeId);
-        if(theme == null){
-            return res.status(404).send(({
-                error: 'not_found',
-                error_description: 'Theme not found'
-            }));
+export const updateTheme = async (req: Request, res: Response): Promise<Response> => {
+    try {
+       const theme = await Theme.findByIdAndUpdate(req.params.themeId, req.body);
+       res.status(200).json({ theme });
+    } catch (err) {
+        return res.status(500).send({ error: 'server_error', error_description: 'Internal server error' } as ErrorResponse);
+    }
+}
+
+export const deleteTheme = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const theme = await Theme.findByIdAndDelete(req.params.themeId);
+        if (theme == null) {
+            return res.status(404).send({ error: 'not_found', error_description: 'Theme not found' } as ErrorResponse);
         }
         return res.status(204).send();
-       }catch(err){
-            return res.status(500).send(err)
-       }
-   }
+    } catch (err) {
+        return res.status(500).send({ error: 'server_error', error_description: 'Internal server error' } as ErrorResponse);
+    }
+}
