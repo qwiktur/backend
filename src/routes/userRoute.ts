0b3d47fd30f5d72f2925cdeb2ca8fff15abaf6dd
Lelberto <jsurieux@gmail.com>
2020-12-11 15:09:53 +0100

    Remove grant access This is temporary

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express'
-import { grantAccess, allowIfLoggedin, getUser, getUsers, updateUser, deleteUser, userInfo } from '../controllers/userController';
+import { allowIfLoggedin, getUser, getUsers, updateUser, deleteUser, userInfo } from '../controllers/userController';
 
 const router = Router();
 
 router.get('/info', userInfo);
 
-router.get('/', allowIfLoggedin, grantAccess('readAny', 'profile'), getUsers);
+router.get('/', allowIfLoggedin, getUsers);
 
 router.get('/:userId', allowIfLoggedin, getUser);
 
-router.patch('/:userId', allowIfLoggedin, grantAccess('updateAny', 'profile'), updateUser);
+router.patch('/:userId', allowIfLoggedin, updateUser);
 
-router.delete('/:userId', allowIfLoggedin, grantAccess('deleteAny', 'profile'), deleteUser);
+router.delete('/:userId', allowIfLoggedin, deleteUser);
 
 export default router;
