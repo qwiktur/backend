25bf8a9d4c7d8c1e007d1a32a5a33fed12eec4ea
Lelberto <jsurieux@gmail.com>
2020-12-14 11:48:46 +0100

    Add error management functions Add format errors function to format errors in the good format Add format server errors to format server errors (all server errors are similar) Add Mongoose validation error translation to format these errors in the good format

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -1,9 +1,39 @@
-export interface ErrorResponse {
-    error: Error;
+import { Error } from 'mongoose';
+
+export interface APIError {
+    error: ErrorType;
     error_description: string;
 }
 
-export type Error =
+export interface APIErrorResponse {
+    errors: APIError[];
+}
+
+export const formatErrors = (...errors: APIError[]): APIErrorResponse => {
+    return { errors };
+}
+
+export const formatServerError = (): APIErrorResponse => {
+    return formatErrors({
+        error: 'server_error',
+        error_description: 'Internal server error'
+    });
+}
+
+export const translateMongooseValidationError = (validationError: Error.ValidationError): APIError[] => {
+    const translatedErrors: APIError[] = [];
+    for (const field of Object.keys(validationError.errors)) {
+        const subError = validationError.errors[field];
+        translatedErrors.push({
+            error: 'validation_failed',
+            error_description: subError.message
+        });
+    }
+    return translatedErrors;
+}
+
+export type ErrorType =
       'server_error'
     | 'not_found'
-    | 'forbidden';
+    | 'forbidden'
+    | 'validation_failed';
